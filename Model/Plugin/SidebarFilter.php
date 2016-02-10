9d23c274490a1d6dce1eaa673d8749789de673b7
Silvan Laroo <silvan@sebwite.nl>
2016-02-10 09:42:13 +0100

    Add price filter

---
+++ Model/Plugin/SidebarFilter.php
@@ -0,0 +1,42 @@
+<?php namespace Sebwite\Sidebar\Model\Plugin;
+
+use Magento\Catalog\Model\ResourceModel\Product\Collection as ProductCollection;
+
+/**
+ * This plugin fixes issue with filtration by store/website ID in \Magento\Catalog\Model\ProductRepository::getList()
+ */
+class SidebarFilter
+{
+    /**
+     * aroundAddFieldToFilter method
+     *
+     * @param \Magento\Catalog\Model\ResourceModel\Product\Collection $collection
+     * @param \Closure                                                $proceed
+     * @param                                                         $fields
+     * @param null                                                    $condition
+     *
+     * @return \Magento\Catalog\Model\ResourceModel\Product\Collection
+     */
+    public function aroundAddFieldToFilter(ProductCollection $collection, \Closure $proceed, $fields, $condition = null)
+    {
+        if($fields === 'category_ids') {
+
+            // Get Price
+            $price = isset($_GET['price']) ? $_GET['price'] : false;
+
+            if($price) {
+
+                $price = explode('-', $price);
+
+                // Check if price is set and is numeric
+                if(isset($price[0]) && is_numeric($price[0]) && isset($price[1]) && is_numeric($price[1])) {
+                    $collection->addAttributeToFilter('price', ['gteq' => $price[0]]);
+                    $collection->addAttributeToFilter('price', ['lteq' => $price[1]]);
+                }
+            }
+        }
+
+        /** Do not try to pass empty $fields to addFieldToFilter, it will cause exception */
+        return $fields ? $proceed($fields, $condition) : $collection;
+    }
+}
